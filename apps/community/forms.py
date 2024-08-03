7600e67ee0de0b97d5940ed8e6ebc566e657f5e5
minseok kang <alstjrrkd201@gmail.com>
2024-08-03 23:03:20 +0900

    [fix]동아리 인증 부분 수정

--- apps/community/forms.py
+++ apps/community/forms.py
@@ -22,3 +22,20 @@ class ClubForm(forms.ModelForm):
             "club_intro",
             "club_num",
         ]
+
+class ClubAuthForm(forms.ModelForm):
+
+    club_name = forms.CharField(
+        label="동아리명", widget=forms.TextInput(attrs={"class": "createClub-input"})
+    )
+    auth_code = forms.CharField(
+        label="동아리 인증 번호",
+        widget=forms.TextInput(attrs={"class": "createClub-input"}),
+    )
+
+    class Meta:
+        model = Club
+        fields = [
+            "club_name",
+            "auth_code",
+        ]
--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from apps.landing.forms import SignupForm
+from apps.community.forms import ClubAuthForm
 from .models import User
 from apps.community.models import Club
 
@@ -10,33 +11,29 @@ def landing(request):
 
 
 def club_auth(request):  # 동아리 인증 페이지
-    if request.user.is_authenticated:  # 로그인된 유저만 접속 가능
+    #if request.user.is_authenticated:  # 로그인된 유저만 접속 가능
         if request.method == "GET":
-            return render(request, "landing/club_auth.html")
+            form = ClubAuthForm()
+            return render(request, "landing/club_auth.html",{"form": form})
 
         if request.method == "POST":
-            user = request.user  # 로그인한 유저
-            name = request.POST["club_name"]  # 인증하려는 동아리 이름
-            code = request.POST["auth_code"]  # 유저가 입력한 인증 코드
+            form = ClubAuthForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data["club_name"]  # 폼에서 클럽 이름 가져오기
+                code = form.cleaned_data["auth_code"]  # 폼에서 인증 코드 가져오기
 
-            if (
-                code == None
-            ):  # 코드에 아무것도 입력하지 않은 경우 -> 장고에서 알아서 공백 입력 못하게 할 것 같긴 한데
-                return render(request, "landing/club_auth.html")
+                try:
+                    club = Club.objects.get(club_name=name)  # 클럽 이름으로 클럽 객체 가져오기
+                    if code == club.club_num:  # 코드가 일치하면
+                        request.user.club_id = club  # 해당 유저의 가입된 동아리 정보에 동아리 pk값 입력
+                        request.user.save()
+                        return redirect("community:main")  # 메인 페이지로 이동
+                    else:
+                        form.add_error(None, "인증 코드가 일치하지 않습니다.")  # 폼에 에러 추가
+                except Club.DoesNotExist:
+                    form.add_error(None, "해당 이름의 동아리가 존재하지 않습니다.")  # 폼에 에러 추가
 
-            # 해당 동아리의 club_num(동아리 인증 코드)과 비교하면 됨
-            # club = Club.objects.filter(club_name = name)
-            club = Club.objects.get(club_name=name)  # 하나만 가져올 땐 get이레오
-            # 이거 하고, club name에 unique 걸어야할듯!
-            club_code = club.club_num
-
-            if code == club_code:  # 코드가 일치하면
-                user.club_id = (
-                    club.club_id
-                )  # 해당 유저의 가입된 동아리 정보에 동아리 pk값 입력
-                user.save()
-                return redirect("community:main")  # 메인 페이지로 이동
-            else:
-                return render(request, "landing/club_auth.html")
-    else:
-        return redirect("landing:login")  # 로그인 화면으로 이동
+            return render(request, "landing/club_auth.html", {"form": form})
+    #else:
+    #       return redirect("landing:login")  # 로그인 화면으로 이동
+                
