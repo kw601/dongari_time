2680d5cc415e4a5f875305a498865a0529a18d2b
zkwlr <bluefrog2k@yonsei.ac.kr>
2024-08-03 23:58:17 +0900

    [chore] Register Club, Board, and Post models in admin site

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.shortcuts import render
 from .views import *
 
 app_name = "community"
 
-urlpatterns = []
+urlpatterns = [
+    path("<int:board_id>/", post_list, name="post_list"),
+    path("<int:board_id>/post/<int:post_id>/", post_detail, name="post_detail"),
+    path("<int:board_id>/create/", create_post, name="create_post"),
+]
--- apps/community/views.py
+++ apps/community/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render, redirect
-
-
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Board, Post
+
+
+def post_list(request, board_id):
+    board = get_object_or_404(Board, id=board_id)
+    posts = Post.objects.filter(board_id=board)
+    return render(request, "community/post_list.html", {"board": board, "posts": posts})
+
+
+def post_detail(request, board_id, post_id):
+    # 구현
+    return
+
+
+# @login_required
+def create_post(request, board_id):
+    # 구현
+    return
