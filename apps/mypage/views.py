d915f81f8fc7e313fd4b33e4e1211c33f53ff5e4
minseok kang <alstjrrkd201@gmail.com>
2024-08-09 15:42:45 +0900

    [Add] 로그아웃 부분 세션값 삭제 추가

--- apps/mypage/views.py
+++ apps/mypage/views.py
@@ -4,12 +4,11 @@ from apps.community.models import Club, Comment, Post
 from apps.landing.models import User
 # Create your views here.
 
-def myposts(request):
+def myposts(request): # 내가 작성한 글
     if request.user.is_authenticated:
-        id = request.user.id
-        club_id = request.session.get('club_id') 
-        if request.method == 'GET':
-            posts = Post.objects.filter(user_id = id, club_id = club_id)
-            return render(request, "mypage/myposts.html", {"posts": posts})
+        id = request.user.id # 현재 사용자 pk
+        club_id = request.session.get('club_id') # 현재 접속한 동아리 pk 
+        posts = Post.objects.filter(user_id = id, club_id = club_id) # 현재 접속한 동아리에서 유저가 작성한 모든 post 불러옴
+        return render(request, "mypage/myposts.html", {"posts": posts})
     else:
         return redirect("landing:login")
